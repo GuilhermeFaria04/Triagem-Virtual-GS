da396aad23cadacfc94664c46f4a922954cc973f
GuilhermeFaria <guilhermefaguiar@gmail.com>
2023-11-23 00:38:38 -0300

    Mexendo a App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './style.scss';
+import Login from './Pages/Login';
+import Cadastro from './Pages/cadastro';
+import Paciente from './Pages/Paciente';
+import Triagem from './Pages/Triagem';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Login/>} />
+        <Route path='/cadastro' element={<Cadastro/>}/>
+        <Route path='/paciente' element={<Paciente/>}/>
+        <Route path='/triagem' element={<Triagem/>}/>
+      </Routes>
+   </BrowserRouter>
   );
 }
 
