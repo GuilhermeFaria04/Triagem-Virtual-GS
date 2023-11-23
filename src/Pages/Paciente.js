1c307f8f7704486ecce98eb05fa4733415ab796b
GuilhermeFaria <guilhermefaguiar@gmail.com>
2023-11-23 00:33:55 -0300

    Criando a page paciente

---
+++ src/Pages/Paciente.js
@@ -0,0 +1,99 @@
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import Header from "../Components/Header";
+
+
+
+
+export default function Paciente() {
+    const [cpf, setCpf] = useState('')
+    const [alergia, setAlergia] = useState('')
+    const [tp, setTp] = useState('')
+    const [remedio, setRemedio] = useState('')
+    const [data, setData] = useState('')
+    const [sexo, setSexo] = useState('')
+    const [doencas, setDoencas] = useState('')
+    const [peso, setPeso] = useState('')
+    const [telefone, setTelefone] = useState('')
+
+    const navegacao = useNavigate()
+
+
+    return (
+        <main>
+            <Header />
+            <h1>Dados do Paciente</h1>
+            <form onSubmit={() => navegacao('/triagem')}>
+                <div>
+                    <input
+                        placeholder="Cpf *"
+                        type="text"
+                        value={cpf}
+                        onChange={(e) => setCpf(e.target.value)}
+                        required
+                    />
+                    <input
+                        placeholder="Alergia à remédios"
+                        type="text"
+                        value={alergia}
+                        onChange={(e) => setAlergia(e.target.value)}
+                    />
+                    <input
+                        placeholder="Tipo Sanguineo*"
+                        type="text"
+                        value={tp}
+                        onChange={(e) => setTp(e.target.value)}
+                        maxLength="3"
+                        required
+                    />
+                </div>
+                <div>
+                    <input
+                        placeholder="Remedios de uso diario"
+                        type="text"
+                        value={remedio}
+                        onChange={(e) => setRemedio(e.target.value)}
+                    />
+                    <input
+                        placeholder="Data de nascimento *"
+                        type="date"
+                        value={data}
+                        onChange={(e) => setData(e.target.value)}
+                        required
+                    />
+                    <input
+                        placeholder="Sexo *"
+                        type="text"
+                        value={sexo}
+                        onChange={(e) => setSexo(e.target.value)}
+                        required
+                    />
+                </div>
+                <div>
+                    <input
+                        placeholder="Doenças"
+                        type="text"
+                        value={doencas}
+                        onChange={(e) => setDoencas(e.target.value)}
+                    />
+                    <input
+                        placeholder="Peso *"
+                        type="text"
+                        value={peso}
+                        onChange={(e) => setPeso(e.target.value)}
+                        required
+                    />
+                    <input
+                        placeholder="Telefone de Emergência *"
+                        type="tel"
+                        value={telefone}
+                        onChange={(e) => setTelefone(e.target.value)}
+                        required
+                    />
+                </div>
+                <button type="submit">Salvar Informações</button>
+            </form>
+
+        </main>
+    )
+}
