232f2969c4db9d454166ed1f66cebdcc25533a0a
GuilhermeFaria <guilhermefaguiar@gmail.com>
2023-11-23 00:31:47 -0300

    Criando a page Login

---
+++ src/Pages/Login.js
@@ -0,0 +1,38 @@
+import { useState } from "react"
+import { useNavigate } from "react-router-dom"
+import logo from "../Img/notreLogo.png"
+
+
+export default function Login() {
+    const [email, setEmail] = useState('')
+    const [senha, setSenha] = useState('')
+    const navegacao = useNavigate()
+
+    function login(){
+        navegacao('/vistoria')
+    }
+    return (
+        <div class="auth-container">
+            <img src={logo} alt="Logo NotreDame"/>
+            <form onSubmit={login}>
+            <input 
+                    placeholder="Email *"
+                    type="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    required
+                />
+                <input 
+                    placeholder="Senha *"
+                    type="password"
+                    value={senha}
+                    onChange={(e) => setSenha(e.target.value)}
+                    required
+                    title="Não pode deixar vazio"
+                />
+                <button type="submit">Entrar</button>
+            </form>
+            <p onClick={() => navegacao('/cadastro')}>Não tem uma conta? Registre-se!</p>
+        </div>
+    )
+}
